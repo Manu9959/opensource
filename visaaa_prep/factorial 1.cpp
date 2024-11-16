6a3b266da383f5d4980d3369f6c4eedcbe8e0958
Manu9959 <148534699+Manu9959@users.noreply.github.com>
2024-11-16 20:33:24 +0530

    Create factorial 1.cpp

---
+++ visaaa_prep/factorial 1.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+long long factorial(int n) {
+    if(n == 0){
+        return 1;
+    }
+    return n * factorial(n-1);
+}
+int main() {
+    int n;
+    cin>>n;
+    cout<<factorial(n);
+
+    return 0;
+}
